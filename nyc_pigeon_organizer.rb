f54d505dab4f0826ee3cb1e15756cd16c27252a8
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-02 23:29:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+#nyc_pigeons = {}
+
+  final_data = data.each_with_object({}) do |(key, value), nyc_pigeons|
+    value.map do |key2, names|
+      names.each do |name|
+        if !nyc_pigeons[name]
+          nyc_pigeons[name] = {}
+        end
+        if !nyc_pigeons[name][key]
+          !nyc_pigeons[name][key] = []
+        end
+      nyc_pigeons[name][key].push(key2.to_s)
+      end
+    end
+  end
+  
 end
+
+
+=begin
+pigeon_names = {}
+
+pigeon_names = data.keys
+.each do |key, value|
+  pigeon_names = 
+end
+
+data.collect do |key1, name|
+  pigeon_names << name
+end
+
+=end
